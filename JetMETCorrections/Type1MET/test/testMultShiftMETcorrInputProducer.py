c5bdd630d08b0092325c8527c492c802bb50401d
schoef <robert.schoefbeck@cern.ch>
2015-02-27 12:32:13 +0100

    put miniAOD in testMultShiftMETcorrInputProducer.py

--- JetMETCorrections/Type1MET/test/testMultShiftMETcorrInputProducer.py
+++ JetMETCorrections/Type1MET/test/testMultShiftMETcorrInputProducer.py
@@ -7,9 +7,9 @@ process.source = cms.Source(
 # CSA14 RECO
 #    fileNames = cms.untracked.vstring('root://eoscms.cern.ch//eos/cms/store/relval/CMSSW_7_0_5/RelValTTbar_13/GEN-SIM-RECO/POSTLS170_V6-v3/00000/0423767B-B5DD-E311-A1E0-02163E00E5B5.root') 
 # 720 RECO
-    fileNames = cms.untracked.vstring('root://eoscms.cern.ch//eos/cms/store/relval/CMSSW_7_2_0/RelValTTbar/GEN-SIM-DIGI-RECO/PU_PRE_STA72_V6_FastSim-v2/00000/FE55222C-7359-E411-A563-0025905A60CE.root')
+#    fileNames = cms.untracked.vstring('root://eoscms.cern.ch//eos/cms/store/relval/CMSSW_7_2_0/RelValTTbar/GEN-SIM-DIGI-RECO/PU_PRE_STA72_V6_FastSim-v2/00000/FE55222C-7359-E411-A563-0025905A60CE.root')
 # PHYS14 mAOD
-#    fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU30bx50_PHYS14_25_V1-v1/00000/003B6371-8D81-E411-8467-003048F0E826.root')
+    fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU30bx50_PHYS14_25_V1-v1/00000/003B6371-8D81-E411-8467-003048F0E826.root')
 # PHYS14 mAOD local
 #    fileNames = cms.untracked.vstring('file:/data/schoef/local/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_MINIAODSIM_PU20bx25_PHYS14_25_V1-v1.root')
     )
@@ -17,7 +17,7 @@ process.source = cms.Source(
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'POSTLS170_V6::All'
+process.GlobalTag.globaltag = 'PHYS14_50_V1::All'
 process.load('JetMETCorrections.Type1MET.correctionTermsPfMetMult_cff')
 
 process.out = cms.OutputModule("PoolOutputModule",
@@ -27,6 +27,10 @@ process.out = cms.OutputModule("PoolOutputModule",
      outputCommands = cms.untracked.vstring('keep *') 
 )
 process.load('JetMETCorrections.Type1MET.correctedMet_cff')
+
+#Replacements for mAOD
+process.corrPfMetMult.vertexCollection = cms.untracked.InputTag("offlineSlimmedPrimaryVertices")
+process.corrPfMetMult.srcPFlow = cms.untracked.InputTag("packedPFCandidates")
 #
 # RUN!
 #
